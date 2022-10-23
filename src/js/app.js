68d064df4cbcc41b99157a437eacc850de1029dd
u53f <scalewithusef@gmail.com>
2022-10-23 15:56:38 +0100

    search/default ip data response

--- src/js/app.js
+++ src/js/app.js
@@ -1,45 +1,53 @@
 const App = () => {
   // !Display the Map
-  const mapRender = (coords, zoom) => {
-    const map = L.map("map").setView(coords, zoom);
+  const mapRender = (coords) => {
+    const map = L.map("map").setView(coords, 9);
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
   };
 
-  // mapRender([51.505, -0.09], 13);
+  const renderIpData = async (data) => {
+    const { city, country, geonameId, lat, lng } = data.location;
+    console.log(lat, lng);
+    mapRender([lat, lng], 20);
+  };
 
-  // !Get IP Data
+  // !Search IP Data
   const getIpdata = async (ip) => {
     try {
-      console.log("sdf");
       const req = await fetch(
-        `https://geo.ipify.org/api/v2/country?apiKey=at_htJl99CqfTwgkKxXTvbZcy6ZYDIaE&ipAddress=${ip}`
+        `https://geo.ipify.org/api/v2/country,city?apiKey=at_htJl99CqfTwgkKxXTvbZcy6ZYDIaE&ipAddress=${ip}`
       );
       const data = await req.json();
-      return data;
+      renderIpData(data);
     } catch (err) {
       console.warn(err);
     }
   };
 
-  // !Render IP Data
-  const renderIpData = async () => {
-    const ip = document.querySelector(".ip__form>input").data;
-    const data = await getIpdata("8.8.8.8");
-    console.log(data);
-  };
+  // !Render Default IP
 
-  renderIpData();
+  const renderDefaultdata = async () => {
+    // ! Fetch Client IP
+    const req = await fetch("https://api.ipify.org?format=json");
+    const data = await req.json();
+    getIpdata(data.ip);
+  };
+  renderDefaultdata();
+
+  // !Render IP Search Result
+  const renderIpSearch = () => {
+    const form = document.querySelector(".ip__form");
+    const ip = document.querySelector(".ip__form>input");
+
+    form.addEventListener("submit", async (e) => {
+      e.preventDefault();
+      getIpdata(ip.value);
+    });
+  };
+  renderIpSearch();
 };
 
 App();
-
-// const test = async () => {
-//   const req = await fetch(
-//     "https://geo.ipify.org/api/v2/country?apiKey=at_htJl99CqfTwgkKxXTvbZcy6ZYDIaE&ipAddress=8.8.8.8"
-//   );
-//   const data = await req.json();
-//   console.log(data);
-// };
